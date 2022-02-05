48490ab5d31c57c22f581a89ece1d5b32b6a46cb
Ekam <ekamahujau@gmail.com>
2022-02-06 00:11:28 +1000

    Added middleware for protection using passoport.js (decode jwt and auth users)

--- server/src/routes/userRoute.js
+++ server/src/routes/userRoute.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import protect from '../middlewares/authMiddleware.js';
+import passport from 'passport'
 import {registerUser, loginUser} from '../controllers/userController.js'
 const router = express.Router();
 
@@ -6,6 +8,9 @@ const router = express.Router();
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/data').get(passport.authenticate('jwt', {session: false}), (req, res) =>{
+    res.json(req.user)
+})
 
 
 export default router
