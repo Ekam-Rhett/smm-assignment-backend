93b144f0c538894edfcbfa78e6a460a5c3183402
Ekam <ekamahujau@gmail.com>
2022-02-06 20:36:15 +1000

    Added updated method for updating service in service controller

--- server/src/controllers/serviceController.js
+++ server/src/controllers/serviceController.js
@@ -31,7 +31,6 @@ const createService = asyncHandler(async (req, res) => {
     const {categoryId, supplierServiceId, name, serviceType, retailPrice, quantity, quality, denyLinkDuplicates} = req.body;
 
     const category = await Category.findById(categoryId);
-    console.log(category);
 
     if (category) {
         const newService = await Service.create({
@@ -51,11 +50,11 @@ const createService = asyncHandler(async (req, res) => {
             })
         } else {
             res.status(400);
-            throw new Error("Could not save to database")
+            throw new Error("Could not save to database");
         }
     } else {
         res.status(201);
-        throw new Error('Not valiad categoryId provided')
+        throw new Error('Not valiad categoryId provided');
     }
 });
 
@@ -64,8 +63,8 @@ const deleteService = asyncHandler(async (req, res) => {
     const { serviceId } = req.body;
 
     if (!serviceId) {
-        res.status(400)
-        throw new Error("serviceId is not provided")
+        res.status(400);
+        throw new Error("serviceId is not provided");
     }
 
     const deletedService = await Service.findByIdAndDelete(serviceId);
@@ -73,14 +72,64 @@ const deleteService = asyncHandler(async (req, res) => {
         res.status(201).json({
             serviceId: deletedService._id,
             message: "Successfully deleted"
-        })
+        });
     } else {
-        res.status(400)
-        throw new Error("Could not find service or could not be deleted")
+        res.status(400);
+        throw new Error("Could not find service or could not be deleted");
     }
-})
+});
+
+
+const updateService = asyncHandler(async (req, res) => {
+    const {serviceId, categoryId, supplierServiceId, name, serviceType, retailPrice, quantity, quality, denyLinkDuplicates, isActive} = req.body;
+    const service = await Service.findById(serviceId);
+
+    
+    if (!serviceId) {
+        res.status(400);
+        throw new Error('No serviceId provided');
+    }
+
+    if (categoryId) {
+        const category = await Category.findById(categoryId);
+        if (!category) {
+            res.status(201);
+            throw new Error('Not valiad categoryId provided');
+        }
+
+
+        if (!service) {
+            res.status(400);
+            throw new Error('Service not found')
+        }
+    }
+    
+
+    if (categoryId) service.categoryId = categoryId;
+    if (isActive) service.isActive = isActive;
+    if (supplierServiceId) service.supplierServiceId = supplierServiceId;
+    if (name) service.name = name;
+    if (serviceType) service.serviceType = serviceType;
+    if (retailPrice) service.retailPrice = retailPrice;
+    if (quantity) service.quantity = quantity;
+    if (quality) service.quality = quality;
+    if (denyLinkDuplicates) service.denyLinkDuplicates = denyLinkDuplicates;
+    
+
+    const updatedService = await service.save();
+
+    if  (!updatedService) {
+        res.status(400);
+        throw new Error("Could not update to database")
+    }
+
+    return res.status(201).json({
+        updatedService,
+        message: "Service updated"
+    })
+    
+});
 
 
 
-
-export  {getServices, createService, deleteService};
+export  {getServices, createService, updateService,  deleteService};
--- server/src/routes/serviceRoute.js
+++ server/src/routes/serviceRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import protect from '../middlewares/authMiddleware.js'; // This import is being used by passport.authenticate method
 import passport from 'passport'
-import  { getServices, createService, deleteService } from "../controllers/serviceController.js";
+import  { getServices, createService, deleteService, updateService } from "../controllers/serviceController.js";
 const router = express.Router();
 
 
@@ -9,6 +9,7 @@ const router = express.Router();
 router.route('/all/:showDisabled?').get(getServices);
 router.route('/create').post(passport.authenticate('jwt', {session: false}), createService);
 router.route('/delete').post(passport.authenticate('jwt', {session: false}), deleteService);
+router.route('/update').post(passport.authenticate('jwt', {session: false}), updateService);
 
 
 
