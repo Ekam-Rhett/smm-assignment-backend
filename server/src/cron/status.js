6c4a583ce926b294fba4e9f3b5faaa16915852d8
Ekam Ahuja <ekamahujau@gmail.com>
2022-02-12 16:57:58 +1100

    Fixed a bug in checkBulkStatus() cron job

--- server/src/cron/status.js
+++ server/src/cron/status.js
@@ -4,9 +4,10 @@ import {checkStatus, getServices} from '../helpers/provider.js'
 import Order from '../models/orderModel.js'
 import Service from '../models/serviceModel.js'
 
-export const checkBulkStatus = new CronJob('*/20 * * * *', async function() {
+export const checkBulkStatus = new CronJob('*/20 * * * * *', async function() {
     console.log("Running Cron Task: Checking Order Status'")
-    const orders = await Order.find({ status: {$ne: "Compeleted"}});
+    const orders = await Order.find({ $and: [{status: {$ne: "Completed"}}, {status: {$ne: "Canceled"}}, {status: {$ne: "Refunded"}}]});
+    console.log(orders)
     for (const order of orders) {
         const status = await checkStatus(order.apiOrderId);
         const findOrder = await Order.findOne({apiOrderId: order.apiOrderId})
@@ -19,7 +20,7 @@ export const checkBulkStatus = new CronJob('*/20 * * * *', async function() {
 }, null, true, process.env.TIME_ZONE);
 
 
-export const updatingServiceActive = new CronJob('*/10 * * * * *', async function() {
+export const updatingServiceActive = new CronJob('*/30 * * * *', async function() {
     console.log("Running Cron Task: Disabling Inactive Services & Enabling active ones");
     const services = await Service.find();
     const providerServieces = await getServices();
@@ -35,7 +36,7 @@ export const updatingServiceActive = new CronJob('*/10 * * * * *', async functio
             await queryService.save();
         }
     }
-})
+}, null, true, process.env.TIME_ZONE)
 
 checkBulkStatus.start();
 updatingServiceActive.start()
